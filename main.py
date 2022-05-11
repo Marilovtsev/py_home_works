59bc3ed45cb5c1a8cbe9cee877f581e852d56f1f
Marilovtsev <dimarilovcev@gmail.com>
2022-05-12 00:09:29 +0300

    Add example about transformation

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+# first function
+def greet_user():
+    print("Hello!")
+
+greet_user()
+
+
+# Transfer of information for function
 def greet_user(username):
     print(f"Hello, {username.title()}!")
 
