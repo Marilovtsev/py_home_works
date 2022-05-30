6841c01528d590a7d51a798794bf5c2ca3340683
Marilovtsev <dimarilovcev@gmail.com>
2022-05-30 19:05:39 +0300

    Created a function (greet_users) with list transfer

--- main.py
+++ main.py
@@ -137,15 +137,15 @@
 # print(musician)
 
 
-def build_person(first_name, last_name):
-    """Возвращает словарь с информацией о человеке."""
-    person = {'first': first_name, 'last': last_name}
-    if age:
-        person['age'] = age
-    return person
+# def build_person(first_name, last_name):
+#     """Возвращает словарь с информацией о человеке."""
+#     person = {'first': first_name, 'last': last_name}
+#     if age:
+#         person['age'] = age
+#     return person
+#
+#
+# musician = build_person('jimi', 'hendrix', age=27)
+# print(musician)
 
 
-musician = build_person('jimi', 'hendrix', age=27)
-print(musician)
-
-# 154 использование функции в цикле while
