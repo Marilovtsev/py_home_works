98f1db7084045d7b7e1ce4e063a9b6ff83111596
Marilovtsev <dimarilovcev@gmail.com>
2022-06-03 17:59:16 +0300

    Using an arbitrary set of named arguments

---
+++ user_profile.py
@@ -0,0 +1,11 @@
+def build_profile(first, last, **user_info):
+    """Строит словарь с информацией о пользователе"""
+    user_info['first_name'] = first
+    user_info['last_name'] = last
+    return user_info
+
+
+user_profile = build_profile('james', 'shepard',
+                             location='andromeda',
+                             field='commander')
+print(user_profile)
