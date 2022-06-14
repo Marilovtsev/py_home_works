833ff3c19f8c9d5127b869bf6e0753b44384cab2
Marilovtsev <dimarilovcev@gmail.com>
2022-06-14 21:58:41 +0300

    Create Dog class

---
+++ dog.py
@@ -0,0 +1,15 @@
+class Dog():
+    """Простая модель собаки"""
+
+    def __init__(self, name, age):
+        """Инициализирует атрибуты name и age"""
+        self.name = name
+        self.age = age
+
+    def sit(self):
+        """Собака садиться по команде"""
+        print(f"{self.name} is now sitting.")
+
+    def roll_over(self):
+        """Собака перекатывается по команде"""
+        print(f"{self.name} rolled over!")
