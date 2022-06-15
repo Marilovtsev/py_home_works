efe4573b3fa052c30eae08a63724c3b3a3dbd049
Marilovtsev <dimarilovcev@gmail.com>
2022-06-15 21:59:25 +0300

    Creating my_dog instance of a class

--- dog.py
+++ dog.py
@@ -13,3 +13,9 @@ class Dog():
     def roll_over(self):
         """Собака перекатывается по команде"""
         print(f"{self.name} rolled over!")
+
+
+my_dog = Dog('willie', 6)
+
+print(f"My dog's name is {my_dog.name}")
+print(f"My dog is {my_dog.age} years old.")
