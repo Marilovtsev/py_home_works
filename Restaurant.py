8ae577e28120f94272f6c2d0397f488f34fbf324
Marilovtsev <dimarilovcev@gmail.com>
2022-06-23 20:34:05 +0300

    Added method open_restaurant and attributes output

--- Restaurant.py
+++ Restaurant.py
@@ -6,7 +6,11 @@ class Restaurant():
     def describe_restaurant(self):
         print(f"{self.restaurant_name}, {self.cuisine_type}")
 
+    def open_restaurant(self):
+        print(f"Today the {self.restaurant_name} with {self.cuisine_type} is open!")
+
 
 my_restaurant = Restaurant("Traktor Burger", "BBQ")
 
 my_restaurant.describe_restaurant()
+my_restaurant.open_restaurant()
