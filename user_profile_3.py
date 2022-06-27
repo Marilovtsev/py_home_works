c5c6ec81d2bdcba0a93eff53eb71cacf7f8e584f
Marilovtsev <dimarilovcev@gmail.com>
2022-06-27 21:40:20 +0300

    Add greet_user method

--- user_profile_3.py
+++ user_profile_3.py
@@ -11,11 +11,17 @@ class User():
               f"\nAge: {self.age}"
               f"\nEmail: {self.email}")
 
+    def greet_user(self):
+        print(f"\nWelcome to the Normandy {self.first_name} {self.last_name}!"
+              f"\nYour personal email for communication: {self.email}"
+              f"\nAt the time of registration in the ship's system, your age is {self.age} years."
+
 
 user_1 = User("John", "Shepard", 27, "shepard@normandy.com")
 user_2 = User("Liara", "T'shoni", 435, "liara@normandy.com")
 user_3 = User("Garrus", "Vakarian", 112, "garrus@normandy.com")
 
-user_1.describe_user()
-user_2.describe_user()
-user_3.describe_user()
+# user_1.describe_user()
+# user_2.describe_user()
+# user_3.describe_user()
+
