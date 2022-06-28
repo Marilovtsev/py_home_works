55e6eeada9b7800c49e029f09e63b37bcac16e26
Marilovtsev <dimarilovcev@gmail.com>
2022-06-28 22:19:24 +0300

    Create class Car

---
+++ car.py
@@ -0,0 +1,8 @@
+class Car():
+    """Простая модель автомобиля"""
+
+    def __init__(self, make, model, year):
+        """Инициализирует атрибуты описания автомобиля."""
+        self.make = make
+        self.model = model
+        self.year = year
