44ae05823cee0c6076754bd18a4d69cc2f7b4436
Marilovtsev <dimarilovcev@gmail.com>
2022-06-29 21:07:49 +0300

    Add attribute odometer_reading and method read_odometer

--- car.py
+++ car.py
@@ -6,11 +6,16 @@ class Car():
         self.make = make
         self.model = model
         self.year = year
+        self.odometer_reading = 0
 
     def get_descriptive_name(self):
         """Возвращает аккуратно отформатированное оптсание"""
         long_name = f"{self.year} {self.make} {self.model}"
         return long_name.title()
+
+    def read_odometer(self):
+        """Выводит прбег машины в милях"""
+        print(f"This car has {self.odometer_reading} miles on it.")
 
 
 my_new_car = Car('audi', 'a4', 2019)
