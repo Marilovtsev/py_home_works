492d400d4ad12e431d2dc9d654544a217e44e254
Marilovtsev <dimarilovcev@gmail.com>
2022-06-30 21:32:44 +0300

    Added value passing

--- car.py
+++ car.py
@@ -35,6 +35,11 @@ class Car():
 my_used_car = Car('subaru', 'outback', 2015)
 print(my_used_car.get_descriptive_name())
 
+my_used_car.update_odometer(23_500)
+my_used_car.read_odometer()
+
+
+
 
 
 # my_new_car = Car('audi', 'a4', 2019)
