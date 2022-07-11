f9b7dc1ff9f74afc538316d02c4ec56b9ca73997
Marilovtsev <dimarilovcev@gmail.com>
2022-07-11 21:49:33 +0300

    Добавлен атрибут privileges для хранения списка строк вида "разрешено добавлять сообщения", "разрешено удалять пользователей", "разрешено банить пользователей"

--- user_profile_3.py
+++ user_profile_3.py
@@ -24,28 +24,35 @@ class User():
     def reset_login_attempts(self):
         self.login_attempts = 0
 
+
 class Admin(User):
     def __init__(self, first_name, last_name, age, email):
         super().__init__(first_name, last_name, age, email)
 
+    def show_privileges(self, privileges):
+        self.privileges = ['разрешено добавлять сообщения',
+                           'разрешено удалять пользователей',
+                           'разрешено банить пользователей']
+        print(self.privileges)
 
 
+admin = Admin("John", "Shepard", 27, "shepard@normandy.com")
+admin.show_privileges(privileges=())
 
-user_1 = User("John", "Shepard", 27, "shepard@normandy.com")
-user_2 = User("Liara", "T'Soni", 435, "liara@normandy.com")
-user_3 = User("Garrus", "Vakarian", 112, "garrus@normandy.com")
+# user_1 = User("John", "Shepard", 27, "shepard@normandy.com")
+# user_2 = User("Liara", "T'Soni", 435, "liara@normandy.com")
+# user_3 = User("Garrus", "Vakarian", 112, "garrus@normandy.com")
 
 # user_1.describe_user()
 # user_2.describe_user()
 # user_3.describe_user()
 
-user_1.greet_user()
+# user_1.greet_user()
 # user_2.greet_user()
 # user_3.greet_user()
 
-user_1.login_attempts = 1
-user_1.greet_user()
+# user_1.login_attempts = 1
+# user_1.greet_user()
 
-user_1.reset_login_attempts()
-user_1.greet_user()
-
+# user_1.reset_login_attempts()
+# user_1.greet_user()
