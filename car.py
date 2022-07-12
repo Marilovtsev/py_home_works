99bbe19a581e6c2fbc5064a031abc3be37e292a8
Marilovtsev <dimarilovcev@gmail.com>
2022-07-12 21:35:57 +0300

    Модуль Car обновлен, в нем содержится только класс Car

--- car.py
+++ car.py
@@ -1,8 +1,7 @@
-"""Классы для представления машин с бензиновым и электродвигателем."""
+"""Простая модель автомобиля"""
 
 
 class Car():
-    """Простая модель автомобиля"""
 
     def __init__(self, make, model, year):
         """Инициализирует атрибуты описания автомобиля."""
@@ -33,52 +32,6 @@ class Car():
     def increment_odometer(self, miles):
         """Увеличивает показания одометра с заданым приращиванием."""
         self.odometer_reading += miles
-
-
-class Battery():
-    """Простая модель аккумулятора электромобиля"""
-
-    def __init__(self, battery_size=100):
-        """Инициализирует атрибудты аккумулятора"""
-        self.battery_size = battery_size
-
-    def describe_battery(self):
-        """Выводит информацию о мощности аккумулятора."""
-        print(f"This car has a {self.battery_size}-kWh battery.")
-
-    def get_range(self):
-        """Выводит приблизительный запас хода для аккумулятора."""
-        if self.battery_size == 75:
-            range = 260
-        elif self.battery_size == 100:
-            range = 315
-
-        print(f"This car can go about {range} miles on a full charge.")
-
-    def upgrade_battery(self):
-        if self.battery_size == 60:
-            self.battery_size = 85
-            print("Upgraded the battery to 85 kWh.")
-        else:
-            print("The battery is already upgraded.")
-
-
-class ElectricCar(Car):
-    """представляет аспекты машины, спецефические для электромобилей."""
-
-    def __init__(self, make, model, year):
-        """Инициализирует атрибуты класса-родителя.
-        Затем инициализирует атрибуты, специфические для автомобиля."""
-        super().__init__(make, model, year)
-        self.battery = Battery()
-
-    def describe_battery(self):
-        """Выводит информацию о мощности аккумулятора."""
-        print(f"This car has a {self.battery_size}-kWh battery.")
-
-    def fill_gas_tank(self):
-        """У электромобилей нет бензобака."""
-        print("This car doesn't need a gas tank!")
 
 # my_used_car = Car('subaru', 'outback', 2015)
 # print(my_used_car.get_descriptive_name())
