ce27d63563c06dd1cee1d253e8122598b9f41c01
Marilovtsev <dimarilovcev@gmail.com>
2022-07-12 20:43:58 +0300

    Создан отдельный файл с импортом класса Car Добавлен экземпляр для этого класса

--- car.py
+++ car.py
@@ -1,3 +1,5 @@
+"""Класс для представления автомобиля"""
+
 class Car():
     """Простая модель автомобиля"""
 
@@ -32,14 +34,14 @@ class Car():
         self.odometer_reading += miles
 
 
-my_used_car = Car('subaru', 'outback', 2015)
-print(my_used_car.get_descriptive_name())
-
-my_used_car.update_odometer(23_500)
-my_used_car.read_odometer()
-
-my_used_car.increment_odometer(100)
-my_used_car.read_odometer()
+# my_used_car = Car('subaru', 'outback', 2015)
+# print(my_used_car.get_descriptive_name())
+#
+# my_used_car.update_odometer(23_500)
+# my_used_car.read_odometer()
+#
+# my_used_car.increment_odometer(100)
+# my_used_car.read_odometer()
 
 # my_new_car = Car('audi', 'a4', 2019)
 # print(my_new_car.get_descriptive_name())
