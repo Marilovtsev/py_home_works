48f6ead744c3db4c255f3351eebfe4bf83ace44d
Marilovtsev <dimarilovcev@gmail.com>
2022-07-17 18:33:45 +0300

    Импорт функции choise

--- standart_library.py
+++ standart_library.py
@@ -1,3 +1,5 @@
-from random import randint
+from random import choice
 
-print(randint(1, 10))
+players = ['charles', 'martina', 'michael', 'florence', 'eli']
+first_up = choice(players)
+print(first_up.title())
