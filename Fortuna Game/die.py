bbdde0fbf77a02fbcbef6ff35fb621665954a941
Marilovtsev <dimarilovcev@gmail.com>
2022-07-18 20:30:52 +0300

    Моделирование 10 бросков 10-гранного кубика

--- Fortuna Game/die.py
+++ Fortuna Game/die.py
@@ -17,3 +17,12 @@ for roll_num in range(10):
     results.append(result)
 print("10 rolls of a 6-sided die:")
 print(results)
+
+d10 = Die(sides=10)
+
+results = []
+for roll_num in range(10):
+    result = d10.roll_die()
+    results.append(result)
+print("\n10 rolls of a 10-sided die:")
+print(results)
