fa58b3a15229ca5f0fc4085a24a39c84e01b14e2
Marilovtsev <dimarilovcev@gmail.com>
2022-07-19 23:53:15 +0300

    Добавлен метод чтения по строкам

--- Files and exclusions/Reading the entire file/learning_python.py
+++ Files and exclusions/Reading the entire file/learning_python.py
@@ -3,7 +3,7 @@ with open(filename) as python_notes:
     notes_all = python_notes.read()
 
 
-class ReadFile():
+class ReadFile:
     """Инициализирует атрибут notes"""
 
     def __init__(self, notes):
@@ -12,6 +12,15 @@ class ReadFile():
     def read_all(self):
         print(self.notes)
 
+    @staticmethod
+    def read_all_by_lines():
+        with open(filename) as python_lines:
+            for line in python_lines:
+                print(line)
+
 
 notes_1 = ReadFile(notes_all)
 notes_1.read_all()
+
+notes_2 = ReadFile(notes_all)
+notes_2.read_all_by_lines()
