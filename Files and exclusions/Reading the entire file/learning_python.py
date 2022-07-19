a6dc380da3bde4f3fb811671e90a3ef9fd565733
Marilovtsev <dimarilovcev@gmail.com>
2022-07-19 22:26:40 +0300

    Создан класс read_all для вывода всего содержимого файла

--- Files and exclusions/Reading the entire file/learning_python.py
+++ Files and exclusions/Reading the entire file/learning_python.py
@@ -1,5 +1,17 @@
 filename = 'learning_python.txt'
-
 with open(filename) as python_notes:
-    notes = python_notes.read()
-print(notes)
+    notes_all = python_notes.read()
+
+
+class ReadAll():
+    """Инициализирует атрибут notes"""
+
+    def __init__(self, notes):
+        self.notes = notes
+
+    def read_all(self):
+        print(self.notes)
+
+
+notes_1 = ReadAll(notes_all)
+notes_1.read_all()
