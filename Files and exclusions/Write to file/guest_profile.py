10c9b9b0ccb9554fd78a75d67a978189e2de6bc2
Marilovtsev <dimarilovcev@gmail.com>
2022-07-21 20:58:53 +0300

    Некоторые правки

--- Files and exclusions/Write to file/guest_profile.py
+++ Files and exclusions/Write to file/guest_profile.py
@@ -1,6 +1,6 @@
 filename = 'guest.txt'
 
-text = '\nEnter "stop" when you are finished.'
+text = '\nEnter "stop" to exit.'
 text += '\nHi! Tell me your name: '
 
 while True:
@@ -14,8 +14,3 @@ while True:
         with open(filename, 'a') as guest_data:
             guest_data.write(f'\nThe reason why {name.title()} is like programming: {reason.capitalize()}')
             break
-
-
-
-
-
