e2903a6f72b7f5c6dbca622f6f3244b23b154639
Marilovtsev <dimarilovcev@gmail.com>
2022-07-21 20:23:01 +0300

    Создан цикл while, который запрашивает у пользователей имена.

--- Files and exclusions/Write to file/guest_profile.py
+++ Files and exclusions/Write to file/guest_profile.py
@@ -1,5 +1,13 @@
 filename = 'guest.txt'
 
-with open(filename, 'a') as file_object:
-    file_object.write(input("Guest name: "))
+text = '\nEnter "stop" when you are finished.'
+text += '\nHi! Tell me your name: '
 
+while True:
+    name = input(text)
+    if name == 'stop':
+        break
+    else:
+        print('Hello ' + name.title() + '!')
+        with open(filename, 'a') as guest_data:
+            guest_data.write(f'\n{name.title()}')
