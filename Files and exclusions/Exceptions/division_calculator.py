007b101e79e0bf50a7fba30d47b294bbf1fd90fe
Marilovtsev <dimarilovcev@gmail.com>
2022-07-22 20:12:00 +0300

    Полная рабочая модель калькулятора

--- Files and exclusions/Exceptions/division_calculator.py
+++ Files and exclusions/Exceptions/division_calculator.py
@@ -5,4 +5,8 @@ while True:
     first_number = input("\nFirst number: ")
     if first_number == 'q':
         break
-
+    second_number = input("\nSecond number: ")
+    if second_number == 'q':
+        break
+    answer = int(first_number) / int(second_number)
+    print(answer)
